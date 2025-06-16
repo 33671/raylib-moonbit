37d4a1a1fa36876538a07a7171a2d398217642ae
uwu <1434926171@qq.com>
2025-06-16 16:30:33 +0800

    basics

--- src/raylib/stub.c
+++ src/raylib/stub.c
@@ -1,16 +1,29 @@
+#include "raylib.h"
 #include <moonbit.h>
-typedef void* moonbit_closure_t;
+#include <stdint.h>
+#include <sys/types.h>
 
-// `call` indicates how to consume `closure`
-void register_callback(void (*call)(moonbit_closure_t), moonbit_closure_t closure) {
-  moonbit_incref(closure); 
-  // call closure before must insert `moonbit_incref`
-  // https://github.com/moonbitlang/moonbit-docs/issues/664
-  call(closure); // call closure directly
-  
-  moonbit_incref(closure);
-  call(closure);
+void ffi_clear_background(uint8_t *color) {
+  Color c = *(Color *)(color);
+  ClearBackground(c);
+}
+void ffi_draw_text(const char *text, int posX, int posY, int fontSize,
+                   void *color) {
+  Color c = *(Color *)(color);
+  DrawText(text, posX, posY, fontSize, c);
+}
 
-  moonbit_incref(closure);
-  call(closure);
-} 
+void ffi_draw_circle_v(void *ffi_vector2_pos, float radius, uint8_t *color) {
+  DrawCircleV(*(Vector2 *)(ffi_vector2_pos), radius, *(Color *)color);
+}
+
+float *ffi_get_mouse_position(float *pos) {
+  Vector2 v = GetMousePosition();
+  (*pos) = v.x;
+  (*(pos + 1)) = v.y;
+  return pos;
+}
+void ffi_draw_rectangle(int x,int y,int width,int height,uint8_t* color){
+  Color c = *(Color *)(color);
+  DrawRectangle(x, y, width, height, c);
+}
